9b44024c3988f054e2e209250e29079ad800005b
EliasDeHondt <Elias.dehondt@outlook.com>
2024-09-24 18:53:04 +0200

    Refactor Kubernetes Dashboard deployment to apply service account admin YAML

--- Scripts/get-token.sh
+++ Scripts/get-token.sh
@@ -5,4 +5,5 @@
 # @since 24/09/2024        #
 ############################
 
+kubectl apply -f ./service-account-admin.yml -n kubernetes-dashboardard
 kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
