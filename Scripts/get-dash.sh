4e2930ca1ee88cf79b06830c5a8e6916061bc7aa
EliasDeHondt <Elias.dehondt@outlook.com>
2024-09-24 18:12:17 +0200

    Refactor Kubernetes Dashboard deployment and RBAC configuration

--- Scripts/get-dash.sh
+++ Scripts/get-dash.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-#
-# IRSOLS Inc 2019-2021
-# Small script to get K8s Dashboard URL and credential token
-# This is required for multi-host cluster where K8s can randomly
-# assign dashboard pods to any node in the cluster .
-# First version : Aug 2019
-# Last modified : Dec 2020
+############################
+# @author Elias De Hondt   #
+# @see https://eliasdh.com #
+# @since 24/09/2024        #
+############################
 
 export DASH_NODE_NAME=`kubectl get pods  --all-namespaces -o wide | grep "kubernetes-dashboard-" | awk {'print $8'}`
 export DASH_NODE_IP=`grep $DASH_NODE_NAME /etc/hosts | awk {'print $1'}`
@@ -15,4 +13,3 @@ echo "Your Dashboard with Nodeport URL is: https://$DASH_NODE_IP$DASH_NODE_PORT/
 echo
 echo
 kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
-
